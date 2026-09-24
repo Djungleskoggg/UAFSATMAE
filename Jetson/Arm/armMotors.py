6bb9964d0a0881da44e7c246310288e53c4b6b1c
Hudson <hootsoon2@gmail.com>
2026-09-23 19:16:46 -0500

    working arm code

--- Jetson/Arm/armMotors.py
+++ Jetson/Arm/armMotors.py
@@ -1,18 +1,18 @@
 import time
-from roarm-ik import m2Arm
+from roarm_ik import m2Arm
 import pygame as pg
 import serial as ser
 
 pg.init() # initializing pygame so that events exist or somtin - jerick
-serialCon = ser.Serial('/dev/ttyACM0', 9600)
+#serialCon = ser.Serial('/dev/ttyACM0', 9600)
 
 pg.joystick.init()
 joysticks = [pg.joystick.Joystick(x) for x in range(pg.joystick.get_count())]
 print(joysticks)
 clock = pg.time.Clock()
 
-x_joystick = 0
-y_joystick = 0
+x_joystick = 0.0
+y_joystick = 0.0
 
 def main():
     arm = m2Arm(port = "/dev/ttyUSB0")
@@ -20,8 +20,8 @@ def main():
     time.sleep(1)
 
     x = 0.0
-    y = 0.0
-    z = 0.0
+    y = 10.0
+    z = 10.0
 
     while(True):
         for event in pg.event.get():
@@ -29,22 +29,24 @@ def main():
         x_joystick = round((pg.joystick.Joystick(0).get_axis(0)))  
         y_joystick = round((pg.joystick.Joystick(0).get_axis(1)) * -1)
 
-        serialCon.write(f'{x_joystick* 100} {y_joystick* 100} '.encode("utf-8"))
-        print(f"{x_joystick* 100} {y_joystick* 100}")
+        # serialCon.write(f'{x_joystick* 100} {y_joystick* 100} '.encode("utf-8"))
+        print(f"{x_joystick* 100} {y_joystick* 100} {x} {y} {z}")
 
-        x1_joystick = round((pg.joystick.Joystick(0).get_axis(2)) * .25)  
-        y1_joystick = round((pg.joystick.Joystick(0).get_axis(3)) * -.25)
+        x1_joystick = round((pg.joystick.Joystick(0).get_axis(2)) ,1)*-1  
+        y1_joystick = round((pg.joystick.Joystick(0).get_axis(3)) ,1)
 
         if(pg.joystick.Joystick(0).get_button(4)):
             z = z + .25
-        if(pg.joystick.Joystick(0).get_button(5)):
+        if(pg.joystick.Joystick(0).get_button(3)):
             z = z - .25
 
-        x = x + x1_joystick
-        y = y + y1_joystick
+        if(x1_joystick > 0.25 or x1_joystick < -0.25):
+            x += x1_joystick
+        if(y1_joystick > 0.25 or y1_joystick < -0.25):
+            y += y1_joystick
 
-        arm.move_to_xyz(x,y,z)
-        time.sleep(.1)
+        arm.move_to_xyz(y,x,z)
+        time.sleep(.025)
 
 if __name__ == "__main__":
     main()
