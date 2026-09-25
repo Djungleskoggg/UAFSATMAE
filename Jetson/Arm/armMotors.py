e9264fa0ebfc2d1d60a8cd9ca5e1adc8557e5572
Hudson <hootsoon2@gmail.com>
2026-09-25 14:02:44 -0500

    Working Manual Demo Code

--- Jetson/Arm/armMotors.py
+++ Jetson/Arm/armMotors.py
@@ -4,12 +4,12 @@ import pygame as pg
 import serial as ser
 
 pg.init() # initializing pygame so that events exist or somtin - jerick
-serialCon = ser.Serial('/dev/ttyACM0', 9600)
+serialCon = ser.Serial('/dev/ttyACM0', 115200)
 
 pg.joystick.init()
 joysticks = [pg.joystick.Joystick(x) for x in range(pg.joystick.get_count())]
 print(joysticks)
-clock = pg.time.Clock()
+
 
 x_joystick = 0.0
 y_joystick = 0.0
@@ -17,37 +17,56 @@ y_joystick = 0.0
 def main():
     arm = m2Arm(port = "/dev/ttyUSB0")
     arm.move_init()
-    time.sleep(1)
 
+    alignment = 0.0
+    latch = False
+    
+    clock = pg.time.Clock()
+    
     x = 0.0
     y = 10.0
     z = 10.0
 
+    angle_grip = 10.0
+
     while(True):
         for event in pg.event.get():
             pass
-
+        pg.event.pump()
         x_joystick = round((pg.joystick.Joystick(0).get_axis(0)))  
         y_joystick = round((pg.joystick.Joystick(0).get_axis(1)))
 
-        serialCon.write(f'{x_joystick* 100} {y_joystick* 100} '.encode("utf-8"))
-        print(f"{x_joystick* 100} {y_joystick* 100} {x} {y} {z}")
+        if(pg.joystick.Joystick(0).get_button(13)): # button 13 is the start button on the xbox controller
+            latch = not latch
 
-        x1_joystick = round((pg.joystick.Joystick(0).get_axis(2)) ,1) 
-        y1_joystick = round((pg.joystick.Joystick(0).get_axis(3)) ,1)
+            if latch:
+                alignment = 1
+            else:  
+                alignment = 0
 
-        if(pg.joystick.Joystick(0).get_button(4)):
+        serialCon.write(f'{x_joystick* 100} {y_joystick* 100} {alignment*1}'.encode("utf-8"))
+        print(f"{x_joystick* 100} {y_joystick* 100} {x} {y} {z} {alignment} {angle_grip}")
+
+        x1_joystick = round((pg.joystick.Joystick(0).get_axis(2)) ,1)*-1
+        y1_joystick = round((pg.joystick.Joystick(0).get_axis(3)) ,1)*-1
+
+        if(pg.joystick.Joystick(0).get_hat(0)==(0,1)):  #control z axis movement
             z = z + .25
-        if(pg.joystick.Joystick(0).get_button(3)):
+        if(pg.joystick.Joystick(0).get_hat(0)==(0,-1)):
             z = z - .25
+
+        if(pg.joystick.Joystick(0).get_hat(0)==(1,0)):  #control gripper angle
+            angle_grip = angle_grip + .25
+        if(pg.joystick.Joystick(0).get_hat(0)==(-1,0)):
+            angle_grip = angle_grip - .25
 
         if(x1_joystick > 0.25 or x1_joystick < -0.25):
             x += x1_joystick
         if(y1_joystick > 0.25 or y1_joystick < -0.25):
             y += y1_joystick
 
+        arm.gripper_angle_ctrl(angle_grip,10,3)
         arm.move_to_xyz(y,x,z)
-        time.sleep(.025)
-
+        clock.tick(60)
 if __name__ == "__main__":
     main()
--- Uno/MotorControl/motorCodeTest.ino
+++ Uno/MotorControl/motorCodeTest.ino
@@ -1,43 +1,50 @@
 #include "CytronMotorDriver.h"
 
-// All code is theoretical and untested until I can work with the motors directly.
+const long heartbeat = 3000;
+unsigned long premillis = 0;
 
-/*
-  In this little test code, 
-  Motor 1 is Front left, Motor 2 is Front right
-  Motor 3 is Back Left, Motor 4 is Back Right.
-  This is because we will need 2 of the MDD10A, so in my head
-  the first will be for the front wheels and the second will be the back wheels.
-
-*/
 // Pins were pre-determined on the Figma already so I just copied those pins.
-CytronMD motor1(PWM_DIR, 8, 5); // Motor 1. PWM pin is 8, DIR pin is 5
-CytronMD motor2(PWM_DIR, 9, 4);   // Motor 2. PWM pin is 9, DIR pin is 4
-CytronMD motor3(PWM_DIR, 10, 3);   // Motor 3. PWM pin is 10, DIR pin is 3
-CytronMD motor4(PWM_DIR, 11, 2);   // Motor 4. PWM pin is 11, DIR pin is 2
+CytronMD motor1(PWM_DIR, 4, 7); // Motor 1. PWM pin is 8, DIR pin is 5
+CytronMD motor2(PWM_DIR, 6, 3);   // Motor 2. PWM pin is 9, DIR pin is 4
+CytronMD motor3(PWM_DIR, 11, 12);   // Motor 3. PWM pin is 10, DIR pin is 3
+CytronMD motor4(PWM_DIR, 9, 13);   // Motor 4. PWM pin is 11, DIR pin is 2
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
 }
 
 void loop() {
   // My big ol encompassing if statement that checks to see if the serial connection has any data coming through.
   if(Serial.available() > 0){
+    
     int x = Serial.parseInt();
     int y = Serial.parseInt();
-    
-    // These next if statements are purposefully left with no condition for the purpose of being filled in.
+    int align = Serial.parseInt();
+
+    if( ( -20 < x && x < 20) && ( -20 < y && y < 20) ){ // deadzone stop
+      motorSpeed(0, 0, 0, 0);
+    }
     if( (-45 < x && x < 45) && (y > 45) ){ // straight forward
       motorSpeed(128, 128, 128, 128);
     }
     if( (-45 < x && x < 45) && (y < -45) ){ // straight backwards
       motorSpeed(-128, -128, -128, -128);
     }
-    if( (x > 45) && (-45 < y && y < 45) ){ // sideways right
-      motorSpeed(128, -128, -128, 128);
+    if(align == 1){
+      if( (x > 45) && (-45 < y && y < 45) ){ // sideways right
+        motorSpeed(128, -128, -128, 128);
+      }
+      if( (x < -45) && (-45 < y && y < 45) ){ // sideways left
+        motorSpeed(-128, 128, 128, -128);
+      }
     }
-    if( (x < -45) && (-45 < y && y < 45) ){ // sideways left
-      motorSpeed(-128, 128, 128, -128);
+    if(align == 0){
+      if( (x > 45) && (-45 < y && y < 45) ){ // spin right
+        motorSpeed(128, -128, 128, -128);
+      }
+      if( (x < -45) && (-45 < y && y < 45) ){ // spin left
+        motorSpeed(-128, 128, -128, 128);
+      }
     }
 //    if(){ // diagonal right
 //      motorSpeed(128, 0, 0, 128);
@@ -66,8 +73,8 @@ void loop() {
 //    if(){ // stop
 //      motorSpeed(0, 0, 0, 0);
 //    }
+  }
 }
-
 void motorSpeed(int motorSpeed1, int motorSpeed2, int motorSpeed3, int motorSpeed4){
   motor1.setSpeed(motorSpeed1);
   motor2.setSpeed(motorSpeed2);
